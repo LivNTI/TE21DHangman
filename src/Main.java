f0ee9d2ee805cb45acda46ca63b60774f8e0cc59
LivNTI <107177980+LivNTI@users.noreply.github.com>
2023-08-25 13:59:20 +0200

    Started the hangman project

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ Todo: how should my hangman work?
 
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -25,8 +26,51 @@ public class Main {
         System.out.println("Hello and welcome to hangman!");
         // howInputAndOutputWorks();
 
+        String word = "flaggstångsknoppspoleringsmedel";
+
+        //Create an array of letters
+        char[] letters = word.toCharArray();
+        String[] reveal = new String[word.length()]; // this is where the revealed letter will be displayed
+
+        // fyll reveal med _
+        for (int i = 0; i < reveal.length; i++) {
+            reveal[i] = "_";
+        }
+
+        //String[] testlist = {"hej", "hopp", "hejsan"};
+        //printList(reveal);
+        //printList(testlist);
+        //printList(); // fungerar inte. Metoden måste får en String[] för att fungera!!!
+        //howInputAndOutputWorks(testlist); // vi får inte göra så här!!! metoden tar inte emot några parametrar
+
+        //Create a scanner to be able to ask for user input
+        Scanner myScan = new Scanner(System.in);
+
+        // Ask user for input
+        System.out.println("Guess a letter:");
+        String guess = myScan.nextLine();
+
+        // System.out.println("the guess is " + guess);
+
+        for (int i = 0; i < letters.length; i++) {
+
+            if (guess.equals(String.valueOf(letters[i]))) {
+                System.out.println("Letter " + guess + " is in the word");
+                System.out.println("It is at index" + i);
+
+            }
+        }
+
 
     }
+
+
+    public static void printList(String[] myString) {
+        for (int i = 0; i < myString.length; i++) {
+            System.out.println("at index " + i + " is letter " + myString[i]);
+        }
+    }
+
 
     public static void howInputAndOutputWorks() {
         System.out.println("To print in console typee:");
